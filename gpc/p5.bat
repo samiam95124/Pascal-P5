a10860ac07a031af1c7f555bfdbccf1cf60aa9d4
Scott Moore <samiam@moorecad.com>
2014-08-23 11:29:06 -0700

    Added the ability to specify an input and an output file in the p5 run batch. The input file comes from the PRD file, and the output file comes from the PRR file. The prd file cannot be reset, since that would cause it to back up to the start of the intermediate code. The the PRR file can be rewritten, but neither the prr nor the prd file can change modes between input and output.

--- gpc/p5.bat
+++ gpc/p5.bat
@@ -4,15 +4,21 @@ rem Compile with P5 using GPC
 rem
 rem Execute with:
 rem
-rem p5 <file>
+rem p5 <sourcefile> [<inputfile>[<outputfile>]]
 rem
-rem where <file> is the name of the source file without
+rem where <sourcefile> is the name of the source file without
 rem extention. The Pascal file is compiled and run.
 rem Any compiler errors are output to the screen. Input
 rem and output to and from the running program are from
 rem the console, but output to the prr file is placed
-rem in <file>.out.
+rem in <sourcefile>.out.
+rem
 rem The intermediate code is placed in <file>.p5.
+rem
+rem If <inputfile> and <outputfile> are specified, then these will be
+rem placed as input to the "prd" file, and output from the "prr" file.
+rem Note that the prd file cannot or should not be reset, since that
+rem would cause it to back up to the start of the intermediate code.
 rem
 if not "%1"=="" goto paramok
 echo *** Error: Missing parameter
@@ -22,13 +28,27 @@ if exist "%1.pas" goto fileexists
 echo *** Error: Missing %1.pas file
 goto stop
 :fileexists
+if not "%2"=="" goto continue
+if exist "%2" goto continue1
+echo *** Error: Missing %1 input file
+goto stop
+:continue
 echo.
 echo Compiling and running %1
 echo.
+if not "%2"=="" goto useinputfile
 cp %1.pas prd
+goto compile
+:useinputfile
+rem The input file, if it exists, gets put on the end of the intermediate
+cat %1.pas %2 prd
+:compile
 pcom
 mv prr %1.p5
 cp %1.p5 prd
 pint
+if not "%2"=="" goto stop
+cp %prr %2
+:stop
 rm prd
 rm prr
