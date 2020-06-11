6838cc074be4315da0a8ab735facdbd6a3184b3d
samiam95124 <samiam@moorecad.com>
2020-06-10 20:58:09 -0700

    Fixed typo

--- source/pcom.pas
+++ source/pcom.pas
@@ -31,7 +31,7 @@
 *                                                                              *
 * The views and conclusions contained in the software and documentation are    *
 * those of the authors and should not be interpreted as representing official  *
-* policies, either expressed or implied, of the Pascal-P6 project.             *
+* policies, either expressed or implied, of the Pascal-P5 project.             *
 *                                                                              *
 *                     Portable Pascal assembler/interpreter                    *
 *                     *************************************                    *
